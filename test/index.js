3002402b1ef6a115195ad3fd7c9cfa04ae812173
Ryan Tsao <rtsao@uber.com>
2015-11-05 14:40:34 -0800

    Made exports interface flatter for destructuring

--- test/index.js
+++ test/index.js
@@ -4,19 +4,19 @@ var fs = require('fs');
 var path = require('path');
 var test = require('tape');
 var scopeStyles = require('../');
+var getCss = scopeStyles.getCss;
 
 test('basic functionality', function(t) {
 
   var basicTest = getFixtures('basic');
   var result = scopeStyles(basicTest.source);
-  var expected = basicTest.expected;
 
-  t.equal(result.css, expected, 'css matches expected');
-  t.ok(result.exports, 'exports exists');
-  t.ok(result.exports.foo, 'class foo exists in exports');
-  t.ok(result.exports.bar, 'class bar exists in exports');
-  t.equal(result.exports.foo, 'foo_4gn20', 'foo is correctly scoped');
-  t.equal(result.exports.bar, 'bar_4gn20', 'bar is correctly scoped');
+  t.equal(getCss(result), basicTest.expected, 'css matches expected');
+  t.ok(result, 'exports exists');
+  t.ok(result.foo, 'class foo exists in exports');
+  t.ok(result.bar, 'class bar exists in exports');
+  t.equal(result.foo, 'foo_4gn20', 'foo is correctly scoped');
+  t.equal(result.bar, 'bar_4gn20', 'bar is correctly scoped');
   t.end();
 });
 
